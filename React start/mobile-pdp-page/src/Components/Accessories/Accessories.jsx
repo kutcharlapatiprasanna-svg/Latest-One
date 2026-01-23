00d0c0cd388729323df3ccc9a15e335a6cf155ac
prasanna <kutcharlapatiprasanna@gmail.com>
2026-01-23 21:14:43 +0530

    pdp

--- React start/mobile-pdp-page/src/Components/Accessories/Accessories.jsx
+++ React start/mobile-pdp-page/src/Components/Accessories/Accessories.jsx
@@ -2,6 +2,7 @@ import right from "../../assets/chevron_right.png";
 import backward from "../../assets/chevron_backward.png";
 
 import heells from "../../assets/heelstarp.jpg";
+import settings from "../../assets/settings_3.jpg";
 import "./Accessories.css";
 import { useState } from "react";
 
@@ -9,24 +10,25 @@ const Accessories = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    
+
+
     <div className="accessories">
 
-
+      <div></div>
       <div className="accessories-header">
         <p className="accessss">Accessories</p>
-    
+
         <img
           src={right}
           alt="i"
           onClick={() => setOpen(!open)}
-          className={open ? "rotate" : ""} 
+          className={open ? "rotate" : ""}
         />
       </div>
 
 
       <div className={`productsHiding ${open ? "activing" : "hiding"}`}>
-        <div  className="products-row">
+        <div className="products-row">
 
 
           <img src={backward} className="arrowing" alt="left" />
@@ -63,79 +65,113 @@ const Accessories = () => {
           </div>
 
           <img src={right} className="arrowing" alt="right" />
-       
+
 
         </div>
-          <div className="view-all">
-            View All Accessories
+        <div className="view-all">
+          View All Accessories
+        </div>
+
+        <div className="accessories-header">
+          <p className="accessss">You may also like...
+          </p>
+
+          <img
+            src={right}
+            alt="i"
+            onClick={() => setOpen(!open)}
+            className={open ? "rotate" : ""} />
+        </div>
+
+
+        <div className={`productsHiding ${open ? "activing" : "hiding"}`}>
+          <div className="products-row">
+
+
+            <img src={backward} className="arrowing" alt="left" />
+
+
+            <div className="products">
+
+              <div className="product-card">
+                <div className="img-box">
+                  <img src={heells} alt="Heel Strap" />
+                </div>
+                <div className="info">
+                  <h4>Viper Plus GT Wheelchair </h4>
+                  <p className="item">Item # STDS831</p>
+                  <p className="price">$262.23</p>
+                  <button className="qty">Learn More
+                  </button>
+                  <button className="cart">Add to Cart</button>
+                </div>
+              </div>
+
+              <div className="product-card">
+                <div className="img-box">
+                  <img src={heells} alt="Heel Strap" />
+                </div>
+                <div className="info">
+                  <h4>Viper Plus GT
+                    Wheelchair </h4>
+                  <p className="item">Item # STDS831</p>
+                  <p className="price">$262.23</p>
+                  <button className="qty">Learn More
+                  </button>
+                  <button className="cart">Add to Cart</button>
+                </div>
+              </div>
+
+            </div>
+
+            <img src={right} className="arrowing" alt="right" />
+
+
           </div>
-             
-      <div className="accessories-header">
-        <p className="accessss">You may also like...
-</p>
+
+
+        </div>
+
+
+
+        {/* <div className="last-button-one">
+          <div className="last-page">
+            <img className="last-img" src={settings} alt="s"></img>
+            <p>To find replacement
+              parts or accessories
+              for this product,
+              visit the Parts Finder
+              page.</p>
+            </div>
+           
+              <button className="hi">Search Parts</button>
+            
+        </div>
+
+ */}
     
-        <img
-          src={right}
-          alt="i"
-              onClick={() => setOpen(!open)}
-          className={open ? "rotate" : ""} />
+
+
+    
+
+     <div className="parts-card">
+      <div className="parts-row">
+        <img src={settings} alt="settings" className="parts-icon" />
+
+        <p className="parts-text">
+          To find replacement parts or accessories for this product,
+          visit the Parts Finder page.
+        </p>
       </div>
 
-
-      <div className={`productsHiding ${open ? "activing" : "hiding"}`}>
-        <div  className="products-row">
-
-
-          <img src={backward} className="arrowing" alt="left" />
+      <button className="parts-btn">Search Parts</button>
+    </div>
 
 
-          <div className="products">
-
-            <div className="product-card">
-              <div className="img-box">
-                <img src={heells} alt="Heel Strap" />
-              </div>
-              <div className="info">
-                <h4>Viper Plus GT Wheelchair </h4>
-                <p className="item">Item # STDS831</p>
-                <p className="price">$262.23</p>
-                <button className="qty">Learn More
-</button>
-                <button className="cart">Add to Cart</button>
-              </div>
-            </div>
-
-            <div className="product-card">
-              <div className="img-box">
-                <img src={heells} alt="Heel Strap" />
-              </div>
-              <div className="info">
-                <h4>Viper Plus GT  
-Wheelchair </h4>
-                <p className="item">Item # STDS831</p>
-                <p className="price">$262.23</p>
-                <button className="qty">Learn More
-</button>
-                <button className="cart">Add to Cart</button>
-              </div>
-            </div>
-
-          </div>
-
-          <img src={right} className="arrowing" alt="right" />
-       
-
-        </div>
-          <div className="view-all">
-            View All Accessories
-          </div>
-
-      </div>
 
       </div>
 
 
-   
 
     </div>
 
